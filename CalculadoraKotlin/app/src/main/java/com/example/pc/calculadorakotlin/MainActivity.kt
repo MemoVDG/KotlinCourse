82addedd6f771f2ea41ffe5283733ded3e58b662
MemoVDG <memovdg@gmail.com>
2019-03-03 15:09:27 -0600

    Calculadora terminada

--- CalculadoraKotlin/app/src/main/java/com/example/pc/calculadorakotlin/MainActivity.kt
+++ CalculadoraKotlin/app/src/main/java/com/example/pc/calculadorakotlin/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.pc.calculadorakotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun suma(view:View){
+
+        if(edtFirstNumber.text.toString().isNotBlank() && edtSecondNumber.text.toString().isNotBlank()){
+            txtVResult.text = ((edtFirstNumber.text.toString()).toInt() + (edtSecondNumber.text.toString()).toInt()).toString()
+        }else{
+            Toast.makeText(this,"Introduce el numero plis",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun resta(view:View){
+        if(edtFirstNumber.text.toString().isNotBlank() && edtSecondNumber.text.toString().isNotBlank()){
+            txtVResult.text = ((edtFirstNumber.text.toString()).toInt() - (edtSecondNumber.text.toString()).toInt()).toString()
+        }else{
+            Toast.makeText(this,"Introduce el numero plis",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun multiplicacion(view:View){
+        if(edtFirstNumber.text.toString().isNotBlank() && edtSecondNumber.text.toString().isNotBlank()){
+            txtVResult.text = ((edtFirstNumber.text.toString()).toInt() * (edtSecondNumber.text.toString()).toInt()).toString()
+        }else{
+            Toast.makeText(this,"Introduce el numero plis",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun division(view:View){
+        if(edtFirstNumber.text.toString().isNotBlank() && edtSecondNumber.text.toString().isNotBlank()){
+            txtVResult.text = ((edtFirstNumber.text.toString()).toInt() / (edtSecondNumber.text.toString()).toInt()).toString()
+        }else{
+            Toast.makeText(this,"Introduce el numero plis",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
